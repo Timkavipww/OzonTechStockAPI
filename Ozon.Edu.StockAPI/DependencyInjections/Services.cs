ae189a1f05b54581452b4e11c1260a0bb400834a
viperr familia <easy.timurwin@gmail.com>
2024-12-07 02:49:48 +0300

    firstcommit

---
+++ Ozon.Edu.StockAPI/DependencyInjections/Services.cs
@@ -0,0 +1,25 @@
+﻿namespace Ozon.Edu.StockAPI.DependencyInjections;
+
+public static class Services
+{
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen();
+    }
+}
+public static class ServicesConfigure
+{
+    public static IApplicationBuilder AddConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      
+        return app;
+    }
+}
+public static class SwaggerInjection
+{
+    public static void AddSwaggerConfiguration(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+}
---
+++ Ozon.Edu.StockAPI/Program.cs
@@ -0,0 +1,15 @@
+using Ozon.Edu.StockAPI.DependencyInjections;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddServices(builder.Configuration);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.AddSwaggerConfiguration();
+}
+app.MapGet("/api", () => Results.Redirect("/swagger"));
+app.MapGet("/web", () => Results.Redirect("/swagger"));
+
+app.Run();
