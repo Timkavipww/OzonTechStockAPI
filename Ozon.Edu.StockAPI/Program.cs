ffbeaf227946cf6da5840f461e386edcc41f5fcd
viperr familia <easy.timurwin@gmail.com>
2024-12-07 03:22:02 +0300

    a

--- Ozon.Edu.StockAPI/Program.cs
+++ Ozon.Edu.StockAPI/Program.cs
@@ -14,11 +14,6 @@ app.MapGet("/api", () => Results.Redirect("/swagger"));
 app.MapGet("/web", () => Results.Redirect("/swagger"));
 
 app.Run();
-<<<<<<< HEAD
 
 //TEST BRANCH
 //NO ./ADDasd
-=======
-//MERGE
-//TEST BRANCH
->>>>>>> testbranch
